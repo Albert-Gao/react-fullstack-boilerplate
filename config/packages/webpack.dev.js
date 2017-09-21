6f88d28495ae153a197243264e669921f348184d
Albert Gao <albertgaohy@gmail.com>
2017-09-21 22:05:49 +1200

    add variable determination

--- config/packages/webpack.dev.js
+++ config/packages/webpack.dev.js
@@ -113,6 +113,9 @@ module.exports = {
             }
         ),
         new HtmlWebpackPlugin({ template: paths.appHtml }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin(),
         new WriteFilePlugin(),
